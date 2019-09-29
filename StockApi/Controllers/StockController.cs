ca6be6151eb7536537bd431e45e5123e93466ec5
Manolo Arbulu <manoloarbulu@hotmail.com>
2019-09-29 00:21:51 -0500

    Add new API bot project

---
+++ StockApi/Controllers/StockController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockApi.Model;
+
+namespace StockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        // GET api/stock/apple.us
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            //Consume Stock Endpoint in async way
+            ProcessStockRequest(id);
+            return Ok();
+        }
+
+        private static async void ProcessStockRequest(string id)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/CSV"));
+                client.DefaultRequestHeaders.Add("User-Agent", "Test Stock Bot");
+
+                //Consumes Endpoint and gets stock data
+                var streamTask = await client.GetStringAsync("https://stooq.com/q/l/?s="+id+"&f=sd2t2ohlcv&h&e=csv");
+
+                //Parses received data to proper data members
+                var stockData = new StockModel(streamTask);
+                
+                //Serialize StockData and send Message to Queue
+            }
+        }
+    }
+}
---
+++ StockApi/Model/StockModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace StockApi.Model
+{
+    [DataContract]
+    public class StockModel
+    {
+        [DataMember(IsRequired = true)]
+        public string Symbol { get; set; }
+
+        [DataMember]
+        public string Date { get; set; }
+
+        [DataMember]
+        public string Time { get; set; }
+
+        [DataMember]
+        public string Open { get; set; }
+
+        [DataMember]
+        public string High { get; set; }
+
+        [DataMember]
+        public string Low { get; set; }
+
+        [DataMember]
+        public string Close { get; set; }
+
+        [DataMember]
+        public string Volume { get; set; }
+
+        public StockModel(string receivedData)
+        { }
+    }
+}
